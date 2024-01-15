675f4fa3ac66375c557a6b94252902ffa9163cbc
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-01-15 13:08:53 +0600

    Categories table and collectionView files created.

---
+++ Toffee/Controller/MainController/CategoriesController/TableCell/CategoriesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoriesTableViewCell.swift
+//  Toffee
+//
+//  Created by Md Abir Hossain on 15/1/24.
+//
+
+import UIKit
+
+class CategoriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
